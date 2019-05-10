919c599e73aeec096fabccb369c00804a64b70a8
kairopettai <31885511+kairopettai@users.noreply.github.com>
2019-05-10 15:09:38 +0300

    Add files via upload

---
+++ KT2/src/main/java/KT2/KT2/yl3.java
@@ -0,0 +1,8 @@
+package KT2.KT2;
+
+interface KT2Interface{
+    public String getTextFromFile(String path);
+    public String[] split_words_into_pieces(String text);
+    public String[] turn_words_to_lowercase(String[] text);
+    
+}
